2ac64c5dafdabc1cd2c506bb237002998599bac2
shrtique <shrt@d-test.ru>
2019-01-10 15:48:38 +0500

    continue

--- sources_1/new/axi_lite_median_5x5.sv
+++ sources_1/new/axi_lite_median_5x5.sv
@@ -32,13 +32,13 @@
         input  logic                    i_sys_clk,
         input  logic                    i_sys_aresetn,
 
-		    input  logic [3*DATA_WIDTH-1:0] s_axis_tdata,
+		    input  logic [DATA_WIDTH-1:0]   s_axis_tdata,
         input  logic                    s_axis_tvalid,
         input  logic                    s_axis_tuser,
         input  logic                    s_axis_tlast,
         output logic                    s_axis_tready,
         
-        output logic [8*DATA_WIDTH-1:0] m_axis_tdata,
+        output logic [DATA_WIDTH-1:0]   m_axis_tdata,
         output logic                    m_axis_tvalid,
         output logic                    m_axis_tuser,
         output logic                    m_axis_tlast,
